fb5968a99b8697e8de0f5805db644a33cd426465
Josemsr98 <josemsr9398@gmail.com>
2026-04-29 09:25:15 +0200

    prueba

--- src/main/java/es/ediae/master/programacion/gestionusuario/controller/FakeController.java
+++ src/main/java/es/ediae/master/programacion/gestionusuario/controller/FakeController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+//prueba 1
 @RestController
 @RequestMapping("/fake")
 public class FakeController {
